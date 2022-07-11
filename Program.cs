6abcdb7309c5ab1d95178d2c72f287a6422e656d
Sergey <s.p.web@ya.ru>
2022-07-12 00:39:01 +0300

    2nd task

--- Program.cs
+++ Program.cs
@@ -3,4 +3,15 @@ int firstD = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число ");
 int secondD = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число ");
-int secondD = Convert.ToInt32(Console.ReadLine());
+int thirdD = Convert.ToInt32(Console.ReadLine());
+int Max = firstD;
+if (Max < secondD)
+{
+    Max = secondD;
+}
+if (Max < thirdD)
+{
+    Max = thirdD;
+}
+
+Console.WriteLine("Максимальное число это " + Max);
