12177c038c9a8b0a65d8a0b08fcf2254474ff80c
Sergey <s.p.web@ya.ru>
2022-07-12 00:22:00 +0300

    task1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число ");
+int firstD = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int secondD = Convert.ToInt32(Console.ReadLine());
+
+if (firstD > secondD) 
+{
+    Console.WriteLine("Число " + firstD + " больше числа " + secondD);
+}
+else 
+{
+    Console.WriteLine("Число " + secondD + " больше числа " + firstD);
+}
