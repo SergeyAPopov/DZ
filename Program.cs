717bec4b4fa21796c2263379a7e9bdd1ff05e3e9
Sergey <s.p.web@ya.ru>
2022-07-12 00:31:37 +0300

    task2

--- Program.cs
+++ Program.cs
@@ -2,12 +2,5 @@
 int firstD = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число ");
 int secondD = Convert.ToInt32(Console.ReadLine());
-
-if (firstD > secondD) 
-{
-    Console.WriteLine("Число " + firstD + " больше числа " + secondD);
-}
-else 
-{
-    Console.WriteLine("Число " + secondD + " больше числа " + firstD);
-}
+Console.WriteLine("Введите третье число ");
+int secondD = Convert.ToInt32(Console.ReadLine());
