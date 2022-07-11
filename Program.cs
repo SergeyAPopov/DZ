f86b3cd1b840db76f5d92bf9d994a42fb5a14ef1
Sergey <s.p.web@ya.ru>
2022-07-12 00:44:15 +0300

    task3

--- Program.cs
+++ Program.cs
@@ -1,17 +1,11 @@
-﻿Console.WriteLine("Введите первое число ");
-int firstD = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число ");
-int secondD = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите третье число ");
-int thirdD = Convert.ToInt32(Console.ReadLine());
-int Max = firstD;
-if (Max < secondD)
-{
-    Max = secondD;
-}
-if (Max < thirdD)
-{
-    Max = thirdD;
-}
+﻿Console.WriteLine("Введите число ");
+int Num = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Максимальное число это " + Max);
+if (Num % 2 == 0)
+{
+    System.Console.WriteLine("Число является четным");
+}
+else 
+{
+    System.Console.WriteLine("Число является нечетным");
+}
